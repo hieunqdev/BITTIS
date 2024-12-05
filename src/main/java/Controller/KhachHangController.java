f7e8c7aba0558214dfef4399fed040401dfd9f8d
hieunq23 <hieunq23@fpt.edu.vn>
2024-12-05 10:56:41 +0700

    feature doGet dang ky, fix bug giao dien

---
+++ src/main/java/Controller/KhachHangController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class KhachHangController
+ */
+@WebServlet("/register")
+public class KhachHangController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("view/register.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/webapp/view/register.jsp
+++ src/main/webapp/view/register.jsp
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng Ký</title>
-    <link rel="stylesheet" href="../static/css/register.css">
+    <link rel="stylesheet" href="static/css/register.css">
 </head>
 <body>
     <div class="register-container">
